b6b92e21b368344651794285ba1d13c4a829f564
Ricardojsiscol <ricardojslgg@gmail.com>
2024-07-02 22:15:50 +0200

    Initial options

---
+++ init.lua
@@ -0,0 +1,1 @@
+require("ricardo.core.options")
---
+++ lua/ricardo/core/options.lua
@@ -0,0 +1,41 @@
+vim.cmd("let g:netrw_liststyle = 3")
+
+local opt = vim.opt
+
+-- line numbers
+opt.relativenumber = true
+opt.number = true
+
+-- tabs and indentation
+opt.tabstop = 2 -- 2 spaces for tabs
+opt.shiftwidth = 2 -- 2 spaces for indent width
+opt.expandtab = true -- expand tab to spaces
+opt.autoindent = true -- copy indent from current line when starting a new one
+
+--  line wrapping
+opt.wrap = false -- disable line wrapping
+
+-- search settings
+opt.ignorecase = true -- ignore case when searching
+opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+
+-- cursor line
+opt.cursorline = true -- highlight the current cursor line
+
+-- appearance
+opt.termguicolors = true -- turning on termguicolors needed by colorschemes
+opt.background = "dark" -- always dark colorschemes
+opt.signcolumn = "yes" -- show sign column
+
+-- backspace
+opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
+
+-- clipboard
+opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+
+-- split windows
+opt.splitright = true -- split vertical window to the right
+opt.splitbelow = true -- split horizontal window to the bottom
+
+-- turn off swapfile
+opt.swapfile = false -- They are annoying
